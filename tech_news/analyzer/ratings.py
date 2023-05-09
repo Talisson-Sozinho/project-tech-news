bd085289ecc09fc208ea3f71478f26a4f5f4f87d
Talisson Sozinho <talissonlutero8080@gmail.com>
2023-05-09 05:05:25 -0300

    requisito 10 - feito a função "top_5_categories"

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,23 @@
-# Requisito 10
+from tech_news.database import get_collection
+
+
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    categories_db_population = (
+        get_collection()
+        .find({}, {"_id": False, "category": True})
+        .sort("category", 1)
+    )
+
+    category_summary_quantity = {}
+
+    for news in categories_db_population:
+        if news["category"] in category_summary_quantity:
+            category_summary_quantity[news["category"]] += 1
+        else:
+            category_summary_quantity[news["category"]] = 1
+
+    category_quantity_list_sorted = sorted(
+        category_summary_quantity.items(), key=lambda x: x[1], reverse=True
+    )
+
+    return [category for category, _ in category_quantity_list_sorted[:5]]
